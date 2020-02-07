eb389721b77354c6f5bbf87e7840634bb423aee3
zhangzongyan <1754590086@qq.com>
2020-02-07 09:55:12 +0800

    kernellist2

---
+++ ds_code/kernellist/homework/student.h
@@ -0,0 +1,43 @@
+#ifndef __STUDENT_H
+#define __STUDENT_H
+
+#include <list.h>
+
+// 学生管理系统
+#define TELLENGTH	12
+#define NAMESIZE 	32
+struct stu_st {
+	char name[NAMESIZE];
+	int age;
+	char tel[TELLENGTH];
+};
+
+struct node_st {
+	struct stu_st stu;
+	struct list_head node;
+};
+
+typedef struct list_head sys_student;
+typedef int (*cmp_t)(const struct stu_st *data, const void *key);
+
+// 接口
+// 初始化学生管理系统结构
+int sys_init(sys_student *s);
+
+// 增加
+int sys_add_stu(sys_student *s, struct stu_st stu);
+
+// 删除 所有
+int sys_del_stu(sys_student *s, const void *key, cmp_t cmp);
+
+// 查找
+struct stu_st sys_search_stu(const sys_student *s, const void *key, cmp_t cmp);
+
+// 学生总数
+int sys_sum_stu(const sys_student *s);
+
+// 销毁
+void sys_destroy(sys_student *s);
+
+#endif
+
