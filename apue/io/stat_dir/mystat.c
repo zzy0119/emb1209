0ddc7c7d8184c362dd9138c4c7fc02165d62a664
zhangzongyan <1754590086@qq.com>
2020-02-19 11:58:34 +0800

    stat2

--- apue/io/stat_dir/mystat.c
+++ apue/io/stat_dir/mystat.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,6 +17,12 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	if (S_ISREG(res.st_mode))
+		putchar('-');
+	else if (S_ISDIR(res.st_mode))
+		putchar('d');
+
+#if 0
 	switch(res.st_mode & S_IFMT) {
 		case S_IFREG:
 			putchar('-');
@@ -26,7 +34,38 @@ int main(int argc, char *argv[])
 			puts("i don't know");
 			break;
 	}
-	printf("\n");
+#endif
 
+	// 文件权限 st_mode  arr[] = {-------}
+	if (res.st_mode & S_IRUSR)
+		putchar('r');
+	else
+		putchar('-');
+	if (res.st_mode & S_IWUSR)
+		putchar('w');
+	else
+		putchar('-');
+	if (res.st_mode &S_IXUSR) {
+		if (res.st_mode & S_ISUID)
+			putchar('s');
+		else
+			putchar('x');
+	}else
+		putchar('-');
+
+	// 硬链接个数
+	printf(" %ld ", res.st_nlink);
+
+	// 拥有者 所属组
+	struct passwd *pwd = getpwuid(res.st_uid);
+	if (NULL == pwd)
+		return 1;
+	printf("%s ", pwd->pw_name);
+	struct group *grp = getgrgid(res.st_gid); 
+	if (NULL == grp)
+		return 1;
+	printf("%s ", grp->gr_name);
+
+	printf("\n");
 	return 0;
 }
