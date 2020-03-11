c7050361d70902c56f4503344502d7d7d2a06c94
zhangzongyan <1754590086@qq.com>
2020-03-11 14:54:18 +0800

    0

--- apue/netipc/tcp/recv.c
+++ apue/netipc/tcp/recv.c
@@ -1,21 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#include "proto.h"
 
+#define BUFSIZE	1024
 
 int main(void)
 {
-	socket();
+	int sd, newsd;
+	struct sockaddr_in laddr;
+	msg_t rcvbuf;
+	int cnt;
 
-	bind();
-
-	listen();
-
-	while (1) {
-		accept();
-
-		read(); // recv();
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sd == -1) {
+		perror("socket()");
+		exit(1);
 	}
 
-	close();
+	laddr.sin_family = AF_INET;
+	laddr.sin_port = htons(RCV_PORT);
+	inet_aton(RCV_IP, &laddr.sin_addr);
+	if (bind(sd, (struct sockaddr *)&laddr, sizeof(laddr)) == -1) {
+		perror("bind()");
+		close(sd);
+		exit(1);
+	}
+
+	if (listen(sd, 20) == -1) {
+		perror("listen()");
+		close(sd);
+		exit(1);
+	}
+
+	while (1) {
+		newsd = accept(sd, NULL, NULL);
+		if (newsd == -1) {
+			perror("accept()");
+			close(sd);
+			exit(1);
+		}
+		cnt = read(newsd, &rcvbuf, MSGSIZE); // recv();
+		write(1, rcvbuf.msg, cnt);
+		close(newsd);
+	}
+
+	close(sd);
 
 	exit(0);
 }
--- apue/netipc/tcp/send.c
+++ apue/netipc/tcp/send.c
@@ -1,14 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#include "proto.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	socket();
+	int sd;
+	struct sockaddr_in raddr;
+	msg_t buf;
+
+	if (argc < 2)
+		exit(0);
+
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sd < 0) {
+		perror("socket()");
+		exit(1);
+	}
 
 	// bind();
 
-	connect();
+	raddr.sin_family = AF_INET;
+	raddr.sin_port = htons(RCV_PORT);
+	inet_aton(RCV_IP, &raddr.sin_addr);
+	if (connect(sd, (const struct sockaddr *)&raddr, sizeof(raddr)) == -1) {
+		perror("connect()");
+		close(sd);
+		exit(1);
+	}
 
-	send(); // write();
+
+	strncpy(buf.msg, argv[1], MSGSIZE);
+	send(sd, &buf, strlen(argv[1])+1, 0); // write();
 
 	exit(0);
 }
